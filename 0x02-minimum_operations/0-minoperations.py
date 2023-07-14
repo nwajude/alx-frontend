34cb53d0734e735a5b127e677af0b65fe353925c
nwajude <nwabudikejude1@gmal.com>
2023-07-13 17:25:19 -0700

    dstr

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+"""
+    A method that determines the number of minmum operations given n characters
+
+"""
+
+def minOperations(n):
+    """
+        A function that calculates the fewest number of operations needed to giv        e a result of exactly n H characters in a file
+        args: n: Number of characters to be displayed
+
+        return:
+                number of min operations
+    """
+
+    now = 1
+    start = 0
+    count = 0
+    while now < n:
+        rem = n - now
+        if ( rem % now == 0):
+            start = now
+            now += start
+            count += 2
+        else:
+            now += start
+            count += 1
+    return count
